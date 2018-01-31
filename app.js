27f35f2848c407a44fb4d1e60dc70fa5fc87ff47
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-31 14:34:19 -0500

    Cargar archivos usando AJAX y JSON

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* Cargar archivos usando AJAX y JSON */
  
-const btnEmpleado = document .getElementById( 'boton1' );       // Obtiene el elemento con la clase 'boton1'
+const btnEmpleado = document .getElementById( 'boton1' ),       // Obtiene el elemento con la clase 'boton1'
+      btnEmpleados = document .getElementById( 'boton2' );      // Obtiene el elemento con la clase 'boton2'
 
 // Evento 'click' para el elemento que representa botón 'EMPLEADO' en el DOM
 btnEmpleado .addEventListener( 'click', function() {
@@ -31,6 +32,49 @@ btnEmpleado .addEventListener( 'click', function() {
                   document .getElementById( 'empleado' ) .innerHTML = htmlTemplate;     // innerHTML / appendChild()
 
             console .log( this .responseText );
+            
+        }
+    }
+
+    // Envia el Request
+    xhr .send();
+});
+
+// Evento 'click' para el elemento que representa botón 'EMPLEADO' en el DOM
+btnEmpleados .addEventListener( 'click', function() {
+    const xhr = new XMLHttpRequest();
+
+    // Abre la conexión
+    xhr .open(
+        'GET',               // Método: GET, POST, PUT, DELETE...
+        'empleados.json',    // Path: del archivo JSON
+        true                 // [true/false] Si es asíncrono
+    );
+
+    // OnLoad: Una ves establecida la conexión
+    xhr .onload = function() {
+        if( this .status === 200 ) {
+            const personal = JSON .parse( this .responseText );      // Convierte JSON a 'Array' de Objetos JavaScript
+                  
+            htmlTemplate = '';      // Crea un Template vacío
+            // Recorre cada uno de los objetos que están en el 'Array'
+            personal .forEach( function( persona ) {
+                // Agrega estructura al Template
+                htmlTemplate += ` 
+                <ul>
+                    <li><b>ID:</b> ${ persona .id }</li>
+                    <li><b>Nombre:</b> ${ persona .nombre }</li>
+                    <li><b>Empresa:</b> ${ persona .empresa }</li>
+                    <li><b>Actividad:</b> ${ persona .trabajo }</li>
+                </ul>
+                `;
+
+                // Agrega el Template con los datos en el DOM
+                document .getElementById( 'empleados' ) .innerHTML = htmlTemplate;     // innerHTML / appendChild()
+
+            });
+
+            console .log( personal );
             
         }
     }
